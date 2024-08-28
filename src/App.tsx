3625beca8c2e18089574d9ee6e3a762a0a74820e
Obed Nuertey <101027384+obedNuertey1@users.noreply.github.com>
2024-08-28 13:11:22 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = ():JSX.Element => {
+  return (
+    <div className='card rounded-none'>
+      Sample tailwind demonstration
+    </div>
+  );
+};
+
+export default App;
